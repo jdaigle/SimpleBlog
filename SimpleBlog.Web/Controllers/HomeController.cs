ad854b01473ad2f49d60263ad54cc1d0255ad3cb
Joseph Daigle <joseph@cridion.com>
2009-08-02 17:30:52 -0400

    Initial Commit

---
+++ SimpleBlog.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Web.Controllers
+{
+    [HandleError]
+    public partial class HomeController : Controller
+    {
+        
+        public virtual ActionResult Index()
+        {           
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+    }
+}
